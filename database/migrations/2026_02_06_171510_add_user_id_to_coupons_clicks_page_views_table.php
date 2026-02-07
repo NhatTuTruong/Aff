40e7398428a64fadcbc860302d4a7a38f82ccf55
Bonmby <nhatdt2020@gmail.com>
2026-02-07 23:51:32 +0700

    update

--- database/migrations/2026_02_06_171510_add_user_id_to_coupons_clicks_page_views_table.php
+++ database/migrations/2026_02_06_171510_add_user_id_to_coupons_clicks_page_views_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('coupons_clicks_page_views', function (Blueprint $table) {
-            //
+            
         });
     }
 
